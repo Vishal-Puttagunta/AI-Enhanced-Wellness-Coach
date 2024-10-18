dd611adc45c5f579035cf77233d82de7240bc213
Vishal Puttagunta <vis.putt@gmail.com>
2024-10-18 19:56:30 -0400

    commit

--- Main.java
+++ Main.java
@@ -4,11 +4,21 @@ public class Main{
         Scanner input = new Scanner(System.in);
 
         //prompt user to enter height and weight
+        System.out.print("What is your height unit preference: ");
+        String heightPreference = input.next();
+
         System.out.print("Enter your height: ");
         
         
     }
 
     //method for bmi
-    
+    public static void calculateBMI(String sex, double height, double weight, int age) {
+        if(heightPreference.equals("ft")){)
+            double heightInMeters = (0.3048*height);
+        }
+        else() {
+            heightInMeters = height;
+        }
+    }
 }
