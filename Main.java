e28dd8e39abf887163eed9ecfff97f3ea96b0979
Vishal Puttagunta <vis.putt@gmail.com>
2024-10-18 19:48:35 -0400

    added Scanner

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        //prompt user to enter height and weight
+        System.out.print("Enter your height: ");
+        
+        
+    }
+
+    //method for bmi
+    
+}
