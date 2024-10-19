6278cd023fcd2c812c5eea01a6231f830331a8e4
Vishal Puttagunta <vis.putt@gmail.com>
2024-10-18 20:33:34 -0400

    added more comments

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+
+        //initializing the scanner
         Scanner input = new Scanner(System.in);
 
         //prompt user to enter height and weight
         System.out.print("What is your height unit preference (imperial or metric): ");
         String heightPreference = input.next();
+
+        //initializing height to empty value
         double height = 0;
 
+        //two if statements for whichever system the user uses
         if(heightPreference.equals("imperial")){
             System.out.print("Enter your height in inches: ");
             height = input.nextDouble();
@@ -18,14 +23,19 @@ public class Main{
             height = input.nextDouble();
         }
 
+
+        //prompting user to enter which preference for weight
         System.out.print("What is your weight preference (kg or lb): ");
         String weightPrefrence = input.next();
 
-
+        //asking user to enter their weight
         System.out.print("Enter your weight: ");
         double weight = input.nextDouble();
 
+        //displaying the final bmi
         System.out.print("Your BMI is: ");
+
+        //formatting the BMI to two decimals
         System.out.printf("%.2f", calculateBMI(heightPreference, weightPrefrence, height, weight));
         
         
@@ -33,8 +43,10 @@ public class Main{
 
     //method for bmi
     public static double calculateBMI(String heightPreference, String weightPreference, double height, double weight) {
-        double heightInMeters = 0;
+        //initializing variable to empty value
         double weightInKG = 0;
+
+        //converting the height into meters for the final calculation
         if(heightPreference.equals("imperial")){
             height = height*0.0254;
         }
@@ -42,7 +54,7 @@ public class Main{
             height = height/100;
         }
 
-
+        //converting the weight into kilograms
         if(weightPreference.equals("lb")){
             weightInKG = (weight/2.25);
         }
@@ -50,7 +62,10 @@ public class Main{
             weightInKG = weight;
         }
 
+        //calculating the BMI using formula
         double bmi = weightInKG/(height*height);
+
+        //returning final value
         return(bmi);
     }
 }
