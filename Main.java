e684f1371e8c433e23d611ceefb69bae0e6b5309
Vishal Puttagunta <vis.putt@gmail.com>
2024-10-18 21:37:56 -0400

    Added new method for score

--- Main.java
+++ Main.java
@@ -57,6 +57,7 @@ public class Main{
         Score = 35.      Person is not in shape, will get an easier or less intensive routine
         */
         
+        System.out.println(calculateScore("Male", "Sedentary", 20, 60));
     }
 
     //method for bmi
@@ -85,5 +86,61 @@ public class Main{
 
         //returning final value
         return(bmi);
+    }
+
+    public static int calculateScore(String sex, String activityLevel, double bmi, int motivationLevel){
+        //initializing the points variable
+        int points = 0;
+
+        //if statements for the BMI category
+        if(bmi < 18.5) {
+            points += 10;
+        }
+        else if(bmi >= 18.5 && bmi <= 25) {
+            points += 20;
+        }
+        else if(bmi > 25 && bmi <= 30) {
+            points += 15;
+        }
+        else if(bmi > 30) {
+            points += 5;
+        }
+
+
+        //if statements for sex
+        if(sex.equals("Male")){
+            points += 6;
+        }
+        if(sex.equals("Female")){
+            points += 4;
+        }
+
+
+        //if statements for activity level
+        if(activityLevel.equals("Sedentary")){
+            points += 5;
+        }
+        if(activityLevel.equals("Lightly active")){
+            points += 15;
+        }
+        if(activityLevel.equals("Moderately active")){
+            points += 20;
+        }
+        if(activityLevel.equals("Very active")){
+            points += 30;
+        }
+
+        //if statements for motivation
+        if(motivationLevel < 20){
+            points += 5;
+        }
+        else if(motivationLevel >=20 && motivationLevel < 70){
+            points += 15;
+        }
+        else if(motivationLevel >= 70){
+            points += 30;
+        }
+
+        return(points);
     }
 }
